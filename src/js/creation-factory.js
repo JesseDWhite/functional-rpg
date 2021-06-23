01df51db347cf96d139f2eeef29d0a11887fbfdb
Jesse White <jesse.white6@gmail.com>
2021-06-23 16:19:59 -0700

    :boom: add starting function factory and character creation.
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

---
+++ src/js/creation-factory.js
@@ -0,0 +1,19 @@
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  };
+};
+
+export const stateControl = storeState();
+
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value
+    });
+  };
+};
--- src/main.js
+++ src/main.js
@@ -2,7 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { stateControl } from './js/creation-factory';
+import { changeState } from './js/creation-factory';
 
 $(document).ready(function () {
+  $("#character-create").submit(function (event) {
+    event.preventDefault();
+    const characterName = changeState("name")($("#name").val());
+    const characterClass = changeState("class")($("#class").val());
+    const characterStrength = changeState("strength")($("#strength").val());
+    const characterEndurance = changeState("endurance")($("#endurance").val());
+    const characterIntelligence = changeState("#intelligence")($("#interlligence").val());
+    const characterDexterity = changeState("#dexterity")($("#dexterity").val());
+    const characterLuck = changeState("#luck")($("#luck").val());
+  });
+
+
+  // I need to be able to pass in the value of each property above into the creation-factory function
+
 
 });
+
+//function factory that can create new instances of either characters, creatures, and or itmes/weapons
+//declare a few premade items and creatures const greenHerb = someFunction(health)(5) yourCharacter(someFunction(greenHerb))
