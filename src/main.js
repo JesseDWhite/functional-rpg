5822a603b84b5bf0d50bd6c73cb3f4e39f16ed2f
Jesse White <jesse.white6@gmail.com>
2021-06-24 13:03:04 -0700

    :boom: add code for creating a character and saveing it to the DOM.
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- src/main.js
+++ src/main.js
@@ -12,10 +12,34 @@ $(document).ready(function () {
     const characterClass = changeState("class")($("#class").val());
     const characterStrength = changeState("strength")($("#strength").val());
     const characterEndurance = changeState("endurance")($("#endurance").val());
-    const characterIntelligence = changeState("#intelligence")($("#interlligence").val());
-    const characterDexterity = changeState("#dexterity")($("#dexterity").val());
-    const characterLuck = changeState("#luck")($("#luck").val());
-  });
+    const characterIntelligence = changeState("intelligence")($("#interlligence").val());
+    const characterDexterity = changeState("dexterity")($("#dexterity").val());
+    const characterLuck = changeState("luck")($("#luck").val());
+
+    const createdCharacter = stateControl(characterName) {
+      return stateControl(characterClass) {
+        return stateControl(characterStrength) {
+          return stateControl(characterEndurance) {
+            return stateControl(characterIntelligence) {
+              return stateControl(characterDexterity) {
+                return stateControl(characterLuck) {
+                }
+              }
+            }
+          }
+        }
+      }
+  }
+
+
+
+
+    $("#current-stats").text(createdCharacter.class);
+
+  console.log(`name: ${createdCharacter.name} class: ${createdCharacter.class} strength: ${createdCharacter.strength}`);
+  console.log(createdCharacter);
+});
+
 
 
   // I need to be able to pass in the value of each property above into the creation-factory function
